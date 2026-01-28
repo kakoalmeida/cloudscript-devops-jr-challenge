e54f0b1f8e65d0bb3805bf9dcdef3f758dcd5acd
Kako <kako.rd.ad@gmail.com>
2026-01-28 01:29:40 -0300

    feat: Add module for managed node group

--- modules.tf
+++ modules.tf
@@ -12,3 +12,13 @@ module "eks_cluster" {
   subnet_public_1a = module.eks_network.subnet_public_1a
   subnet_public_1b = module.eks_network.subnet_public_1b
 }
+
+module "managed_node_group" {
+  source         = "./modules/managed-node-group"
+  project_name   = var.project_name
+  cluster_name   = module.eks_cluster.cluster_name
+  ec2_type       = var.ec2_type
+  subnet_priv_1a = module.eks_network.subnet_priv_1a
+  subnet_priv_1b = module.eks_network.subnet_priv_1b
+  tags           = var.tags
+}
