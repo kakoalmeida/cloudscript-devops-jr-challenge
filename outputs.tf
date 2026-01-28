9d6c1f36a61727538cf6c0643ffc290c30bb002f
Kako <kako.rd.ad@gmail.com>
2026-01-28 00:32:33 -0300

    feat: Output subnets for eks module

---
+++ outputs.tf
@@ -0,0 +1,15 @@
+output "subnet_public_1a" {
+  value = module.eks_network.subnet_public_1a
+}
+
+output "subnet_public_1b" {
+  value = module.eks_network.subnet_public_1b
+}
+
+output "subnet_priv_1a" {
+  value = module.eks_network.subnet_priv_1a
+}
+
+output "subnet_priv_1b" {
+  value = module.eks_network.subnet_priv_1b
+}
