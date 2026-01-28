32f5cb388ec8ccbdb03e8eb391bca24dda761224
Kako <kako.rd.ad@gmail.com>
2026-01-28 00:33:16 -0300

    feat: add EKS cluster module

--- modules.tf
+++ modules.tf
@@ -4,3 +4,11 @@ module "eks_network" {
   project_name = var.project_name
   tags         = var.tags
 }
+
+module "eks_cluster" {
+  source           = "./modules/cluster-eks"
+  project_name     = var.project_name
+  tags             = var.tags
+  subnet_public_1a = module.eks_network.subnet_public_1a
+  subnet_public_1b = module.eks_network.subnet_public_1b
+}
