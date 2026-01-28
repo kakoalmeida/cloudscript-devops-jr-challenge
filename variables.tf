6cf4767e6c1fea6543d01c6716c39e1a07213dea
Kako <kako.rd.ad@gmail.com>
2026-01-27 23:54:50 -0300

    feat: add vars for vpc module

--- variables.tf
+++ variables.tf
@@ -1,3 +1,18 @@
 variable "aws_region" {
   type = string
 }
+
+variable "project_name" {
+  type        = string
+  description = "Project name to identify VPC"
+}
+
+variable "cidr_block" {
+  type        = string
+  description = "IPv4 CIDR block for VPC"
+}
+
+variable "tags" {
+  type        = map(any)
+  description = "A map of tags to add to all resources."
+}
