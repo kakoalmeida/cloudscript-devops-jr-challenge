f8ae5b490a69dc622be09db0c58ae782700e7a5c
Kako <kako.rd.ad@gmail.com>
2026-01-28 00:02:50 -0300

    feat: add outputs for pub subnets

--- modules/network/outputs.tf
+++ modules/network/outputs.tf
@@ -9,3 +9,11 @@ output "subnet_priv_1b" {
 output "vpc_id" {
   value = aws_vpc.cluster_eks_vpc.id
 }
+
+output "subnet_public_1a" {
+  value = aws_subnet.eks_subnet_public_1a.id
+}
+
+output "subnet_public_1b" {
+  value = aws_subnet.eks_subnet_public_1b.id
+}
